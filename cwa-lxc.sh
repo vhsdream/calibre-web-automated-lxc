b37f241bc020c569e185d4633a7af18894e872da
vhsdream <punk.sand7393@fastmail.com>
2025-05-16 07:40:01 -0400

    First pass at Autocaliweb version

--- cwa-lxc.sh
+++ cwa-lxc.sh
@@ -14,8 +14,8 @@ usage() {
   header
   cat <<EOF
 Functions:
-'install'   Installs Calibre-Web Automated on a Debian 12 LXC in Proxmox.
-'update'    Checks for updates to Calibre-Web Automated upstream and applies them to an existing installation.
+'install'   Installs AutoCaliWeb on a Debian 12 LXC in Proxmox.
+'update'    Checks for updates to AutoCaliWeb upstream and applies them to an existing installation.
 
 Available options:
 -h, --help      Print this help and exit
@@ -43,7 +43,7 @@ header() {
  ██        ██▄██▄██  ▄██▀▀▀██   █████      ██        ▄██▄    ██
  ▀██▄▄▄▄█  ▀██  ██▀  ██▄▄▄███              ██▄▄▄    ▄█▀▀█▄   ▀██▄▄▄▄█
    ▀▀▀▀▀    ▀▀  ▀▀    ▀▀▀▀ ▀▀               ▀▀▀▀   ▀▀▀  ▀▀▀    ▀▀▀▀▀
-${CLR}${YELLOW}A helper script for Calibre-Web Automated in a Proxmox LXC${CLR}
+${CLR}${YELLOW}A helper script for AutoCaliWeb in a Proxmox LXC${CLR}
 EOF
     )"
   fi
@@ -74,7 +74,7 @@ setup_colours
 
 # Flash and bling
 # app() {
-#   echo -e "${CLR}${PURPLE}cwa${CLR}"
+#   echo -e "${CLR}${PURPLE}acw${CLR}"
 # }
 
 bling() {
@@ -152,20 +152,21 @@ parse_params() {
 parse_params "$@"
 
 # Global vars
-OLD_BASE="/app/calibre-web-automated"
-BASE="/opt/cwa"
+OLD_BASE="/app/autocaliweb"
+BASE="/opt/acw"
 SCRIPTS="$BASE/scripts"
-APP="$BASE/root/app/calibre-web/cps"
+APP="$BASE/cps"
 OLD_CONFIG="/config"
-CONFIG="/var/lib/cwa"
+CONFIG="/var/lib/acw"
 OLD_DB="$OLD_CONFIG/app.db"
-DB="/opt/calibre-web/.calibre-web/app.db"
+DB="$CONFIG/app.db"
 OLD_META_TEMP="$OLD_BASE/metadata_temp"
 META_TEMP="$CONFIG/metadata_temp"
 OLD_META_LOGS="$OLD_BASE/metadata_change_logs"
 META_LOGS="$CONFIG/metadata_change_logs"
-INGEST="cwa-book-ingest"
-CONVERSION=".cwa_conversion_tmp"
+LIBRARY="calibre-library"
+INGEST="acw-book-ingest"
+CONVERSION=".acw_conversion_tmp"
 
 # Main functions
 install() {
@@ -203,89 +204,83 @@ install() {
   ./kepubify-linux-64bit --version | awk '{print substr($2 ,2)}' >/opt/kepubify/version.txt
   msg_done "Installed Kepubify!"
 
-  msg_start "Installing uv & creating virtualenv..."
+  msg_start "Installing uv..."
   export UV_INSTALL_DIR="/usr/bin"
   $shh bash -c "$(curl -fsSL https://astral.sh/uv/install.sh)"
-  uv -q venv /opt/venv
-  msg_done "uv installed, venv created!"
+  msg_done "uv installed!"
 
   msg_start "Installing Calibre..."
   $shh apt-get install -y calibre --no-install-recommends
   msg_done "Calibre installed!"
 
-  msg_start "Installing Calibre-Web..."
-  mkdir -p /opt/calibre-web
-  wget -q https://github.com/janeczku/calibre-web/raw/master/library/metadata.db -P /opt/calibre-web
-  cd /opt/calibre-web
-  source /opt/venv/bin/activate
-  uv -q pip install calibreweb[goodreads,metadata,kobo]
-  uv -q pip list | grep calibreweb | awk '{print $2}' >/opt/calibre-web/calibreweb_version.txt
-  msg_done "Installed Calibre-Web!"
-
   # Create calibre user
-  useradd -U -s /usr/sbin/nologin -M -d /opt/calibre-web calibre
-  chown -R calibre:calibre /opt/{venv,calibre-web}
+  useradd -U -s /usr/sbin/nologin -M -d "$BASE" calibre
+  chown -R calibre:calibre "$BASE"
 
-  # Create service file
+  Create service file
   cat <<EOF >/etc/systemd/system/cps.service
-  [Unit]
-  Description=Calibre-Web Server
-  After=network.target
+    [Unit]
+    Description=Calibre-Web Server
+    After=network.target
 
-  [Service]
-  Type=simple
-  User=calibre
-  Group=calibre
-  WorkingDirectory=/opt/calibre-web
-  ExecStart=/opt/venv/bin/cps
-  TimeoutStopSec=20
-  KillMode=process
-  Restart=on-failure
+    [Service]
+    Type=simple
+    User=calibre
+    Group=calibre
+    WorkingDirectory=/opt/acw
+    ExecStart=/opt/acw/venv/bin/cps
+    TimeoutStopSec=20
+    KillMode=process
+    Restart=on-failure
 
-  [Install]
-  WantedBy=multi-user.target
+    [Install]
+    WantedBy=multi-user.target
 EOF
 
-  msg_start "Starting and then stopping Calibre-Web Service..."
+  # msg_start "Starting and then stopping Calibre-Web Service..."
   # necessary otherwise the patching operation will fail
-  systemctl start cps && sleep 5 && systemctl stop cps
-  msg_done "Calibre-Web Service successfully cycled."
+  # systemctl start cps && sleep 5 && systemctl stop cps
+  # msg_done "Calibre-Web Service successfully cycled."
 
-  msg_start "Installing Calibre-Web Automated..."
+  msg_start "Installing AutoCaliWeb..."
   cd /tmp
-  RELEASE=$(curl -s https://api.github.com/repos/crocodilestick/Calibre-Web-Automated/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-  wget -q "https://github.com/crocodilestick/Calibre-Web-Automated/archive/refs/tags/V$RELEASE.zip" -O cwa.zip
-  unzip -q cwa.zip
-  mv Calibre-Web-Automated-"$RELEASE"/ /opt/cwa
-  cd /opt/cwa
+  RELEASE=$(curl -s https://api.github.com/repos/gelbphoenix/autocaliweb/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  curl -fsSLO "https://github.com/gelbphoenix/autocaliweb/archive/refs/tags/v$RELEASE.zip"
+  unzip -q v"$RELEASE".zip
+  mv autocaliweb-"$RELEASE"/ "$BASE"
+  cd "$BASE"
+  uv -q venv venv
+  source ./venv/bin/activate
+  sed -n "/^goodreads/p; /^jsonschema/p; /^rarfile/,/^pycountry/p" ./optional-requirements.txt >./options.txt
   uv -q pip install -r requirements.txt
+  uv -q pip install -r options.txt
   deactivate
-  msg_done "Calibre-Web Automated installed!"
+  msg_done "AutoCaliWeb installed!"
 
   msg_start "Starting patching operations..."
-  mkdir -p /opt/cwa-book-ingest
-  mkdir -p /var/lib/cwa/{metadata_change_logs,metadata_temp,processed_books,log_archive,.cwa_conversion_tmp}
-  mkdir -p /var/lib/cwa/processed_books/{converted,imported,failed,fixed_originals}
-  touch /var/lib/cwa/convert-library.log
+  mkdir -p /opt/{acw-book-ingest,calibre-library}
+  mkdir -p /var/lib/acw/{metadata_change_logs,metadata_temp,processed_books,log_archive,.acw_conversion_tmp}
+  mkdir -p /var/lib/acw/processed_books/{converted,imported,failed,fixed_originals}
+  touch /var/lib/acw/convert-library.log
 
   # patcher functions
   replacer
   script_generator
-  chown -R calibre:calibre "$BASE" "$CONFIG" /opt/{"$INGEST",kepubify,calibre-web,venv}
+  chown -R calibre:calibre "$BASE" "$CONFIG" /opt/{"$INGEST","$LIBRARY",kepubify}
   msg_done "Patching operations successful!"
 
   msg_start "Creating & starting services & timers, confirming a successful start..."
-  cat <<EOF >/etc/systemd/system/cwa-autolibrary.service
+  cat <<EOF >/etc/systemd/system/acw-autolibrary.service
   [Unit]
-  Description=Calibre-Web Automated Auto-Library Service
+  Description=AutoCaliWeb Auto-Library Service
   After=network.target cps.service
 
   [Service]
   Type=simple
   User=calibre
   Group=calibre
-  WorkingDirectory=/opt/cwa
-  ExecStart=/opt/venv/bin/python3 /opt/cwa/scripts/auto_library.py
+  WorkingDirectory=/opt/acw
+  ExecStart=/opt/acw/venv/bin/python3 /opt/acw/scripts/auto_library.py
   TimeoutStopSec=10
   KillMode=process
   Restart=on-failure
@@ -294,17 +289,17 @@ EOF
   WantedBy=multi-user.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/cwa-ingester.service
+  cat <<EOF >/etc/systemd/system/acw-ingester.service
   [Unit]
-  Description=Calibre-Web Automated Ingest Service
-  After=network.target cps.service cwa-autolibrary.service
+  Description=AutoCaliWeb Ingest Service
+  After=network.target cps.service acw-autolibrary.service
 
   [Service]
   Type=simple
   User=calibre
   Group=calibre
-  WorkingDirectory=/opt/cwa
-  ExecStart=/usr/bin/bash -c /opt/cwa/scripts/ingest-service.sh
+  WorkingDirectory=/opt/acw
+  ExecStart=/usr/bin/bash -c /opt/acw/scripts/ingest-service.sh
   TimeoutStopSec=10
   KillMode=mixed
   Restart=on-failure
@@ -313,17 +308,17 @@ EOF
   WantedBy=multi-user.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/cwa-change-detector.service
+  cat <<EOF >/etc/systemd/system/acw-change-detector.service
   [Unit]
-  Description=Calibre-Web Automated Metadata Change Detector Service
-  After=network.target cps.service cwa-autolibrary.service
+  Description=AutoCaliWeb Metadata Change Detector Service
+  After=network.target cps.service acw-autolibrary.service
 
   [Service]
   Type=simple
   User=calibre
   Group=calibre
-  WorkingDirectory=/opt/cwa
-  ExecStart=/usr/bin/bash -c /opt/cwa/scripts/change-detector.sh
+  WorkingDirectory=/opt/acw
+  ExecStart=/usr/bin/bash -c /opt/acw/scripts/change-detector.sh
   TimeoutStopSec=10
   KillMode=mixed
   Restart=on-failure
@@ -332,36 +327,36 @@ EOF
   WantedBy=multi-user.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/cwa.target
+  cat <<EOF >/etc/systemd/system/acw.target
   [Unit]
-  Description=Calibre-Web Automated Services
+  Description=AutoCaliWeb Services
   After=network-online.target
-  Wants=cps.service cwa-autolibrary.service cwa-ingester.service cwa-change-detector.service cwa-autozip.timer
+  Wants=cps.service acw-autolibrary.service acw-ingester.service acw-change-detector.service acw-autozip.timer
 
   [Install]
   WantedBy=multi-user.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/cwa-autozip.service
+  cat <<EOF >/etc/systemd/system/acw-autozip.service
   [Unit]
-  Description=Calibre-Web Automated Nightly Auto-Zip Backup Service
+  Description=AutoCaliWeb Nightly Auto-Zip Backup Service
   After=network.target cps.service
 
   [Service]
   Type=simple
   User=calibre
   Group=calibre
-  WorkingDirectory=/var/lib/cwa/processed_books
-  ExecStart=/opt/venv/bin/python3 /opt/cwa/scripts/auto_zip.py
+  WorkingDirectory=/var/lib/acw/processed_books
+  ExecStart=/opt/acw/venv/bin/python3 /opt/acw/scripts/auto_zip.py
   Restart=on-failure
 
   [Install]
   WantedBy=multi-user.target
 EOF
 
-  cat <<EOF >/etc/systemd/system/cwa-autozip.timer
+  cat <<EOF >/etc/systemd/system/acw-autozip.timer
   [Unit]
-  Description=Calibre-Web Automated Nightly Auto-Zip Backup Timer
+  Description=AutoCaliWeb Nightly Auto-Zip Backup Timer
   RefuseManualStart=no
   RefuseManualStop=no
 
@@ -369,27 +364,27 @@ EOF
   Persistent=true
   # run every day at 11:59PM
   OnCalendar=*-*-* 23:59:00
-  Unit=cwa-autozip.service
+  Unit=acw-autozip.service
 
   [Install]
   WantedBy=timers.target
 EOF
 
-  cd scripts
-  chmod +x check-cwa-services.sh ingest-service.sh change-detector.sh
-  echo "V${RELEASE}" >/opt/cwa/version.txt
-  systemctl -q enable --now cwa.target
+  cd "$SCRIPTS"
+  chmod +x check-acw-services.sh ingest-service.sh change-detector.sh
+  echo "${RELEASE}" >/opt/acw/version.txt
+  systemctl -q enable --now acw.target
   $shh apt autoremove
   $shh apt autoclean
-  rm -f /tmp/cwa.zip
+  rm -f /tmp/acw.zip
   sleep 3
-  local services=("cps" "cwa-ingester" "cwa-change-detector")
+  local services=("cps" "acw-ingester" "acw-change-detector")
   local status=""
   status=$(for service in "${services[@]}"; do
     systemctl is-active "$service" | grep active -
   done)
   if [[ "$status" ]]; then
-    msg_done "Calibre-Web Automated is live!"
+    msg_done "AutoCaliWeb is live!"
     sleep 1
     LOCAL_IP=$(hostname -I | awk '{print $1}')
     msg_info "Go to ${YELLOW}http://$LOCAL_IP:8083${CLR} to login"
@@ -397,16 +392,16 @@ EOF
     msg_info "${PURPLE}Default creds are user: 'admin', password: 'admin123'${CLR}"
     exit 0
   else
-    die "Something's not right, please check CWA services!"
+    die "Something's not right, please check acw services!"
   fi
 }
 
 replacer() {
   cd $BASE
   # Deal with a couple initial modifications
-  sed -i "s|\"/calibre-library\"| \"/opt/calibre-web\"|" dirs.json ./scripts/auto_library.py
-  sed -i -e "s|\"$OLD_CONFIG/$CONVERSION\"| \"$CONFIG/$CONVERSION\"|" \
-    -e "s|\"/$INGEST\"| \"/opt/$INGEST\"|" dirs.json
+  sed -i "s|\"/$LIBRARY\"|\"/opt/$LIBRARY\"|" dirs.json ./scripts/auto_library.py
+  sed -i -e "s|\"$OLD_CONFIG/$CONVERSION\"|\"$CONFIG/$CONVERSION\"|" \
+    -e "s|\"/$INGEST\"|\"/opt/$INGEST\"|" dirs.json
 
   # Gather list of Python scripts to be iterated
   FILES=$(find ./scripts "$APP" -type f -name "*.py" -or -name "*.html")
@@ -425,75 +420,75 @@ replacer() {
 
   # Deal with edge case(s)
   sed -i -e "s|\"/admin$CONFIG\"|\"/admin$OLD_CONFIG\"|" \
-    -e "s|app/LSCW_RELEASE|opt/calibre-web/calibreweb_version.txt|g" \
-    -e "s|app/CWA_RELEASE|opt/cwa/version.txt|g" \
-    -e "s/lscw_version/calibreweb_version/g" \
+    -e "s|app/ACW_RELEASE|opt/acw/version.txt|g" \
     -e "s|app/KEPUBIFY_RELEASE|opt/kepubify/version.txt|g" \
-    -e "s|app/cwa_update_notice|opt/.cwa_update_notice|g" \
+    -e "s|app/acw_update_notice|opt/.acw_update_notice|g" \
     $APP/admin.py $APP/render_template.py
-  sed -i "s|\"$CONFIG/post_request\"|\"$OLD_CONFIG/post_request\"|; s|python3|/opt/venv/bin/python3|g" $APP/cwa_functions.py
+  # -e "s|app/LSCW_RELEASE|opt/calibre-web/calibreweb_version.txt|g"
+  # -e "s/lscw_version/calibreweb_version/g"
+  sed -i "s|\"$CONFIG/post_request\"|\"$OLD_CONFIG/post_request\"|; s|python3|/opt/acw/venv/bin/python3|g" $APP/acw_functions.py
   sed -i -e "/^# Define user/,/^os.chown/d" -e "/nbp.set_l\|self.set_l/d" -e "/def set_libr/,/^$/d" \
     ./scripts/{convert_library.py,kindle_epub_fixer.py,ingest_processor.py}
   sed -i "/chown/d" ./scripts/auto_library.py
-  sed -i -n '/Linuxserver.io/{x;d;};1h;1!{x;p;};${x;p;}' $APP/templates/admin.html &&
-    sed -i -e "/Linuxserver.io/,+3d" \
-      -e "s/commit/calibreweb_version/" $APP/templates/admin.html
+  # sed -i -n '/Linuxserver.io/{x;d;};1h;1!{x;p;};${x;p;}' $APP/templates/admin.html &&
+  #   sed -i -e "/Linuxserver.io/,+3d" \
+  #     -e "s/commit/calibreweb_version/" $APP/templates/admin.html
 
   # patch the calibre-web python libs in the virtualenv
-  cp -r /opt/cwa/root/app/calibre-web/cps/* /opt/venv/lib/python3*/site-packages/calibreweb/cps
+  # cp -r /opt/acw/root/app/calibre-web/cps/* /opt/venv/lib/python3*/site-packages/calibreweb/cps
 }
 
 script_generator() {
-  bash -c "cat > /opt/cwa/scripts/change-detector.sh" <<-EOF
+  bash -c "cat > /opt/acw/scripts/change-detector.sh" <<-EOF
 #!/usr/bin/env bash
 
 echo "========== STARTING METADATA CHANGE DETECTOR ==========="
 
 # Folder to monitor
-WATCH_FOLDER="/var/lib/cwa/metadata_change_logs"
+WATCH_FOLDER="/var/lib/acw/metadata_change_logs"
 echo "[metadata-change-detector] Watching folder: \$WATCH_FOLDER"
 
 # Monitor the folder for new files
 inotifywait -m -e close_write -e moved_to --exclude '^.*\.(swp)$' "\$WATCH_FOLDER" |
 while read -r directory events filename; do
         echo "[metadata-change-detector] New file detected: \$filename"
-        /opt/venv/bin/python3 /opt/cwa/scripts/cover_enforcer.py "--log" "\$filename"
+        /opt/acw/venv/bin/python3 /opt/acw/scripts/cover_enforcer.py "--log" "\$filename"
 done
 EOF
 
   bash -c "cat > $SCRIPTS/ingest-service.sh" <<-EOF
 #!/usr/bin/env bash
 
-echo "========== STARTING CWA-INGEST SERVICE =========="
+echo "========== STARTING acw-INGEST SERVICE =========="
 
-WATCH_FOLDER=$(grep -o '"ingest_folder": "[^"]*' /opt/cwa/dirs.json | grep -o '[^"]*$')
-echo "[cwa-ingest-service] Watching folder: \$WATCH_FOLDER"
+WATCH_FOLDER=$(grep -o '"ingest_folder": "[^"]*' /opt/acw/dirs.json | grep -o '[^"]*$')
+echo "[acw-ingest-service] Watching folder: \$WATCH_FOLDER"
 
 inotifywait -m -r --format="%e %w%f" -e close_write -e moved_to "\$WATCH_FOLDER" |
 while read -r events filepath ; do
-        echo "[cwa-ingest-service] New files detected - \$filepath - Starting Ingest Processor..."
-        /opt/venv/bin/python3 /opt/cwa/scripts/ingest_processor.py "\$filepath"
+        echo "[acw-ingest-service] New files detected - \$filepath - Starting Ingest Processor..."
+        /opt/acw/venv/bin/python3 /opt/acw/scripts/ingest_processor.py "\$filepath"
 done
 EOF
 
-  bash -c "cat > $SCRIPTS/check-cwa-services.sh" <<-EOF
+  bash -c "cat > $SCRIPTS/check-acw-services.sh" <<-EOF
 #!/usr/bin/env bash
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo "====== Calibre-Web Automated -- Status of Monitoring Services ======"
+echo "====== AutoCaliWeb -- Status of Monitoring Services ======"
 echo ""
 
-INGESTER_STATUS=\$(systemctl is-active cwa-ingester)
-METACHANGE_STATUS=\$(systemctl is-active cwa-change-detector)
+INGESTER_STATUS=\$(systemctl is-active acw-ingester)
+METACHANGE_STATUS=\$(systemctl is-active acw-change-detector)
 
 if [ "\$INGESTER_STATUS" = "active" ] ; then
-    echo -e "- cwa-ingest-service \${GREEN}is running\${NC}"
+    echo -e "- acw-ingest-service \${GREEN}is running\${NC}"
     is=true
 else
-    echo -e "- cwa-ingest-service \${RED}is not running\${NC}"
+    echo -e "- acw-ingest-service \${RED}is not running\${NC}"
     is=false
 fi
 
@@ -508,10 +503,10 @@ fi
 echo ""
 
 if \$is && \$mc; then
-    echo -e "Calibre-Web-Automated was \${GREEN}successfully installed \${NC}and \${GREEN}is running properly!\${NC}"
+    echo -e "AutoCaliWeb was \${GREEN}successfully installed \${NC}and \${GREEN}is running properly!\${NC}"
     exit 0
 else
-    echo -e "Calibre-Web-Automated was \${RED}not installed successfully\${NC}, please check the logs for more information."
+    echo -e "AutoCaliWeb was \${RED}not installed successfully\${NC}, please check the logs for more information."
     if [ "\$is" = true ] && [ "\$mc" = false ] ; then
         exit 1
     elif [ "\$is" = false ] && [ "\$mc" = true ] ; then
